5ffdb987a97824657dfa97062cce02946a56fb72
GuardianCoding <youssef.hassan2606@gmail.com>
2025-10-21 19:35:22 +1000

    Updated Dataset download script to fix incorrect URL

--- recognition/MambaIRv2 Greyscale to Colour - Youssef Hassan (48621573)/scripts/download_datasets.sh
+++ recognition/MambaIRv2 Greyscale to Colour - Youssef Hassan (48621573)/scripts/download_datasets.sh
@@ -3,15 +3,11 @@ set -e
 mkdir -p datasets && cd datasets
 
 echo "↓ Downloading DFWB_RGB (HQ)..."
-gdown --id 1pmyHyyKbgz6J_v5cSVE7R5oS7nhaC6YE -O DFWB_RGB_HQ.zip
+gdown 1jPgG_URDQZ4kyXaMMXJ8AZ8jEErCdKuM -O DFWB_RGB_HQ.zip
 unzip -q DFWB_RGB_HQ.zip && rm DFWB_RGB_HQ.zip
 
 echo "↓ Downloading ColorDN test sets..."
-gdown --id 1U9sahbwc0_So3kHmyvXocslTT-wG0gyB -O ColorDN.zip
+gdown 1baLpOjNlTCNbREUDAZf9Lso6YCeUOQER -O ColorDN.zip
 unzip -q ColorDN.zip && rm ColorDN.zip
-
-echo "↓ (optional) Downloading DF2K HR..."
-gdown --id 1VGcJpH5H07Fi3jVdEx1A09G7W2Aj5yTb -O DF2K_HR.zip || true
-unzip -q DF2K_HR.zip 2>/dev/null && rm -f DF2K_HR.zip
 
 echo "Done!"
